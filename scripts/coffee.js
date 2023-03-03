a61c5e6918870e2ecd29cad8476f031cfc09933e
erinknight242 <erinknight242@gmail.com>
2023-03-03 11:46:45 -0600

    Improve coffee's frequency

--- scripts/coffee.js
+++ scripts/coffee.js
@@ -2,7 +2,7 @@ const { random } = require('../utils/utils');
 
 module.exports = (app) => {
   app.message('coffee', async ({ message, say }) => {
-    if (random() >= 7)
+    if (random(100) >= 70)
     await say("Coffee? How about a beer? :beer:");
   });
 }
